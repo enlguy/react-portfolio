4ec39baf37ea04b2d1552a2fa69fe43f2e60d41e
Jack <jack282323@gmail.com>
2023-02-22 11:25:36 +0100

    revert

--- src/App.js
+++ src/App.js
@@ -1,94 +1,56 @@
-import Navbar from "./scenes/Navbar";
-import Landing from "./scenes/Landing";
-import DotGroup from "./scenes/DotGroup";
-import MySkills from "./scenes/MySkills";
-import LineGradient from "./components/LineGradient";
-import Projects from "./scenes/Projects";
-import Contact from "./scenes/Contact";
-import Footer from "./scenes/Footer";
-import useMediaQuery from "./hooks/useMediaQuery";
-import { useEffect, useState } from "react";
-import Testimonials from "./scenes/Testimonials";
-import { motion } from "framer-motion";
+import { useState, useEffect } from 'react';
+import useMediaQuery from './hooks/useMediaQuery';
+import Landing from './scenes/Landing';
+import Navbar from './scenes/Navbar';
+import DotGroup from './scenes/DotGroup';
+import Contact from './scenes/Contact';
+import LineGradient from './components/LineGradient';
+import MySkills from './scenes/MySkills';
+import Projects from './scenes/Projects';
+import Footer from './scenes/Footer';
+
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState("home");
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   const [isTopOfPage, setIsTopOfPage] = useState(true);
-  const isDesktop = useMediaQuery("(min-width: 1060px)");
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY === 0) {
-        setIsTopOfPage(true);
-        setSelectedPage("home");
-      }
+      if (window.scrollY === 0) setIsTopOfPage(true);
       if (window.scrollY !== 0) setIsTopOfPage(false);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    }
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar
-        isTopOfPage={isTopOfPage}
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
+        <Navbar 
+          isTopOfPage={isTopOfPage}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
       <div className="w-5/6 mx-auto md:h-full">
-        {isDesktop && (
-          <DotGroup
+        {isAboveMediumScreens && (
+          <DotGroup 
             selectedPage={selectedPage}
             setSelectedPage={setSelectedPage}
           />
         )}
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("home")}
-        >
-          <Landing setSelectedPage={setSelectedPage} />
-        </motion.div>
+        <Landing setSelectedPage={setSelectedPage} />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full ">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("skills")}
-        >
+      <div className="w-5/6 mx-auto md:h-full">
           <MySkills />
-        </motion.div>
       </div>
       <LineGradient />
       <div className="w-5/6 mx-auto">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("projects")}
-        >
           <Projects />
-        </motion.div>
       </div>
       <LineGradient />
       <div className="w-5/6 mx-auto md:h-full">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("testimonials")}
-        >
-          <Testimonials />
-        </motion.div>
-      </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("contact")}
-        >
           <Contact />
-        </motion.div>
       </div>
       <Footer />
     </div>
--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -1,6 +1,7 @@
 import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Footer = () => {
+<<<<<<< HEAD
   return (
     <footer className="h-64 bg-red pt-10">
       <div className="w-10/12 mx-auto">
@@ -17,5 +18,19 @@ const Footer = () => {
     </footer>
   );
 };
+=======
+    return (
+        <footer className="h-64 bg-red pt-10">
+            <div className="w-10/12 mx-auto">
+                <SocialMediaIcons />
+                <div className="md:flex justify-center md:justify-between text-center">
+                    <p className="font-playfair font-semibold text-2xl text-yellow">Jack Epner</p>
+                    <p className="font-playfair text-md text-yellow">©2022 Jack Epner All rights reserved</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+>>>>>>> parent of 2846fbc (Fixing Projects section)
 
-export default Footer;
+export default Footer
