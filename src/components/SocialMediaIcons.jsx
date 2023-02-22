2846fbc286bf9fa16b59ddc987866cf5a66b8926
Jack <jack282323@gmail.com>
2023-02-22 11:15:28 +0100

    Fixing Projects section

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,25 +1,40 @@
-import LinkedInImage from '../assets/linkedin.png';
-import GitHubImage from '../assets/github-original-wordmark.svg';
-
 const SocialMediaIcons = () => {
-    return (
-        <div className="flex justify-center md:justify-start my-10 gap-7">
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://www.linkedin.com/in/jack-epner"
-                target="_blank"
-            >
-                <img alt="linkedin-link" src={LinkedInImage} />
-            </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://github.com/enlguy"
-                target="_blank"
-            >
-                <img alt="github-link" id="githubimage" src={GitHubImage} />
-            </a>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="linkedin-link" src="../assets/linkedin.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.twitter.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="twitter-link" src="../assets/twitter.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="facebook-link" src="../assets/facebook.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="instagram-link" src="../assets/instagram.png" />
+      </a>
+    </div>
+  );
+};
 
-export default SocialMediaIcons
+export default SocialMediaIcons;
--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -1,17 +1,21 @@
 import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Footer = () => {
-    return (
-        <footer className="h-64 bg-red pt-10">
-            <div className="w-10/12 mx-auto">
-                <SocialMediaIcons />
-                <div className="md:flex justify-center md:justify-between text-center">
-                    <p className="font-playfair font-semibold text-2xl text-yellow">Jack Epner</p>
-                    <p className="font-playfair text-md text-yellow">©2022 Jack Epner All rights reserved</p>
-                </div>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="h-64 bg-red pt-10">
+      <div className="w-10/12 mx-auto">
+        <SocialMediaIcons />
+        <div className="md:flex justify-center md:justify-between text-center ">
+          <p className="font-playfair font-semibold text-2xl text-yellow">
+            JANE ESPER
+          </p>
+          <p className="font-playfair text-md text-yellow">
+            ©2022 ESPER. All Rights Reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
